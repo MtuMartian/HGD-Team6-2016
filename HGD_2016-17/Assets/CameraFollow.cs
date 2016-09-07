9e99401ba3048203a91e6496e6fd813b88498045
Miles Martin <miles@rover-229-63.rovernet.mtu.edu>
2016-09-07 15:07:30 -0400

    Added script to the camera to make it smoothly follow the player. This closes #6.

---
+++ HGD_2016-17/Assets/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform playerTransform;
+	public float dampening;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var z = transform.position.z;
+		Vector3 currentPosition = transform.position;
+		Vector3 delta = new Vector3 (
+			                playerTransform.position.x - currentPosition.x,
+			                playerTransform.position.y - currentPosition.y,
+			                currentPosition.z);
+		Vector3 finalPosition = currentPosition + delta;
+		Vector3 smoothMovement = Vector3.SmoothDamp (currentPosition, finalPosition, ref velocity, dampening);
+		transform.position = new Vector3(smoothMovement.x, smoothMovement.y, z);
+	}
+}
