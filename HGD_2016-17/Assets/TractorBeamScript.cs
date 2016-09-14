e18bc7c23356e63d40cbefaeff96124784b8a56a
Miles Martin <miles@rover-231-192.rovernet.mtu.edu>
2016-09-14 15:20:22 -0400

    Added a placeholder tractor beam sprite and script that applies a force to the player while the player is within the tractor beam. This closes #13

---
+++ HGD_2016-17/Assets/TractorBeamScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TractorBeamScript : MonoBehaviour {
+
+	public float strength;
+	private List<GameObject> objects;
+
+	// Use this for initialization
+	void Start () {
+		objects = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		objects.ForEach (obj => {
+			Vector2 force = new Vector2 (
+				-1 * Mathf.Sin (Mathf.Deg2Rad * transform.rotation.eulerAngles.z),
+				1 * Mathf.Cos (Mathf.Deg2Rad * transform.rotation.eulerAngles.z));
+			obj.GetComponent<Rigidbody2D>().AddForce(force * strength);
+		});
+	}
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		objects.Add (obj.gameObject);
+	}
+
+	void OnTriggerExit2D(Collider2D obj) {
+		objects.Remove (obj.gameObject);
+	}
+}
