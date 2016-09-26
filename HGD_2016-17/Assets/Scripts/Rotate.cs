86b03ec96da5642aca2b169372e7f47bfe9dc4c2
mplecapt <mplecapt@mtu.edu>
2016-09-26 18:12:33 -0400

    Made some changes for management meeting
    I added the sprite and normal map to the player, which is now a little
    bigger. I also added a little dynamic lights demo to the start menu and
    added the scenes to the build settings so they can be played in
    succession (although I don't know if that will stick, we may just need
    to manually set those until we start making builds of the game.

---
+++ HGD_2016-17/Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.parent.position, new Vector3 (0, 0, 1), speed * Time.deltaTime);
+	}
+}
