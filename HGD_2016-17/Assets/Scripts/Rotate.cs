2ca1ccc90c620badbbb573966cea147216bf9d86
mplecapt <mplecapt@mtu.edu>
2016-09-28 01:45:54 -0400

    Unity updated
    My unity updated, so it now uses visual studio (which of course means it
    needed to add some file to the project). I am also hoping to test the
    .gitignore situation.

--- HGD_2016-17/Assets/Scripts/Rotate.cs
+++ HGD_2016-17/Assets/Scripts/Rotate.cs
@@ -4,12 +4,6 @@ using System.Collections;
 public class Rotate : MonoBehaviour {
 	public float speed = 1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		transform.RotateAround (transform.parent.position, new Vector3 (0, 0, 1), speed * Time.deltaTime);
 	}
