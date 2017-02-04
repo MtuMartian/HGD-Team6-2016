0b932cc96823c2b53458d1a30da646ac65363eb7
Miles Martin <mrmartin@mtu.edu>
2017-02-03 20:46:40 -0500

    Added an oxygen meter to the game that counts down when the player leaves a gravity sphere.

---
+++ HGD_2016-17/Assets/PlayerOxygen.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Linq;
+
+public class PlayerOxygen : MonoBehaviour 
+{
+    private GameObject player;
+    private PlayerMovementScript playerScript;
+
+    private float OxygenPercentage = 100f;
+    private Canvas canvas;
+    public Image greenBar;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerMovementScript>();
+        canvas = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Draw();
+        if (playerScript.influencingSpheres.Any())
+            IncreaseOxygen();
+        else
+            DecreaseOxygen();
+	}
+
+    void DecreaseOxygen()
+    {
+        if (OxygenPercentage <= 0f)
+        {
+            return;
+        }
+        OxygenPercentage -= 0.1f;
+    }
+
+    void IncreaseOxygen()
+    {
+        if (OxygenPercentage >= 100f)
+            OxygenPercentage = 100f;
+        else
+            OxygenPercentage += 0.5f;
+    }
+
+    void Draw()
+    {
+        greenBar.rectTransform.localScale = new Vector3(OxygenPercentage / 100f, 1, 1);
+        if (OxygenPercentage >= 100f)
+            canvas.enabled = false;
+        else
+            canvas.enabled = true;
+    }
+}
