a93a71d0c72aecf426319775bdf82582876c865f
Miles Martin <mrmartin@mtu.edu>
2017-03-17 20:31:46 -0400

    Added a bezier curve platform to the prefabs.

---
+++ HGD_2016-17/Assets/LineRendererFromBezier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class LineRendererFromBezier : MonoBehaviour {
+
+    private BezierCollider2D collider;
+    private LineRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<BezierCollider2D>();
+        var points = collider.calculate3DPoints();
+
+        renderer = GetComponent<LineRenderer>();
+        renderer.SetVertexCount(points.ToList().Count);
+        renderer.SetPositions(points);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
