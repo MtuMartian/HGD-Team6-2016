cf14329e03c4e5527411626922f38f724d8bef4a
Miles Martin <mrmartin@mtu.edu>
2017-03-24 20:39:04 -0400

    Added bezier curve platforms and began work on official levels.

--- HGD_2016-17/Assets/LineRendererFromBezier.cs
+++ HGD_2016-17/Assets/LineRendererFromBezier.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 public class LineRendererFromBezier : MonoBehaviour {
 
-    private BezierCollider2D collider;
+    private BezierCurveCollider2D collider;
     private LineRenderer renderer;
 
 	// Use this for initialization
 	void Start () {
-        collider = GetComponent<BezierCollider2D>();
-        var points = collider.calculate3DPoints();
+        collider = GetComponent<BezierCurveCollider2D>();
+        var points = collider.getCurvePoints();
+
+        var points3d = new List<Vector3>();
+        points.ForEach(point => {
+            points3d.Add(new Vector3(point.x, point.y, 0));
+        });
 
         renderer = GetComponent<LineRenderer>();
         renderer.SetVertexCount(points.ToList().Count);
-        renderer.SetPositions(points);
+        renderer.SetPositions(points3d.ToArray());
 	}
 	
 	// Update is called once per frame
